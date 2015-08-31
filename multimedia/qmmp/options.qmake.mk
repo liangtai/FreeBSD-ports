0f76c7050c6238e4d803f8908ba7d02c6fa1b7e4
SimaMoto,RyōTa <liangtai.s16@gmail.com>
2015-08-31 22:23:56 +0900

    qmmp-1.0.0, qmmp-plugin-pack-1.0.0

--- multimedia/qmmp/options.qmake.mk
+++ multimedia/qmmp/options.qmake.mk
@@ -2,16 +2,22 @@
 .  if ${PORT_OPTIONS:M${opt}}
 
 .    if defined(${opt}_QMAKE_CONFIG)
-_QT_CONFIG+=	${${opt}_QMAKE_CONFIG}
+_QT_CONFIG_ON+=	${${opt}_QMAKE_CONFIG}
 .    endif
 
 .    if defined(${opt}_QMAKE_CXXFLAGS)
 _QT_CXXFLAGS+=	${${opt}_QMAKE_CXXFLAGS}
 .    endif
 
+.  else
+
+.    if defined(${opt}_QMAKE_CONFIG)
+_QT_CONFIG_OFF+=	${${opt}_QMAKE_CONFIG}
+.    endif
 .  endif
 .endfor
 
-QMAKE_ARGS+=	CONFIG+="${_QT_CONFIG}" \
+QMAKE_ARGS+=	CONFIG+="${_QT_CONFIG_ON}" \
+		DISABLED_PLUGINS="${_QT_CONFIG_OFF}" \
 		QMAKE_CXXFLAGS+="${_QT_CXXFLAGS}" \
 		PREFIX=${PREFIX}
